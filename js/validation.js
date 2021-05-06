23d42f41b46ccf668f50684b356d3f334adf9d3f
Delmiro <delmirodb@gmail.com>
2021-05-06 19:07:48 +0200

    Versión inicial

---
+++ js/validation.js
@@ -0,0 +1,6 @@
+function validar() {
+
+	// En el caso del videojuego no queremos que el navegador cargue una nueva página,
+	// pues perderíamos los valores. Por tanto, la validación siempre debe devolver falso
+	return false;
+}
