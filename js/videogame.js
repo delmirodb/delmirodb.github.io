5e4589612153eaec447cd3c56c80bff880ecc192
Delmiro <delmirodb@gmail.com>
2021-05-06 19:09:42 +0200

    Versión actualizada

--- js/videogame.js
+++ js/videogame.js
@@ -4,12 +4,12 @@ const NUMOBJETIVOS = 48;
 // Clases e IDs de los diferentes elementos HTML
 const CLASEACIERTO = 'hit';
 const CLASEFALLO = 'miss';
-const CLASEVACIO = 'empty';
 const CLASEOBJETIVO = 'target';
 const IDPUNTUACION = 'score';
 const IDNOMBRE = 'name';
 const IDOBJETIVO = 'target';
 const IDFORMULARIO = 'controlPanel';
+const IDRACHA = 'racha';
 
 // Valores por defecto para el estado del juego
 const OBJETIVOSPORMINUTODEF = 60;
@@ -19,6 +19,7 @@ const PUNTOSACIERTODEF = 5;
 const PUNTOSFALLODEF = 5;
 const OBJETIVOACERTADODEF = true;
 const NOMBREJUGADORDEF = '';
+const RACHADEF = 0;
 
 // Objeto global que contiene el estado del juego
 let juego = {
@@ -28,7 +29,8 @@ let juego = {
 	puntosAcierto: PUNTOSACIERTODEF, // Los puntos que se suman por cada objetivo acertado
 	puntosFallo: PUNTOSFALLODEF, // Los puntos que se restan por cada objetivo acertado
 	objetivoPuntuado: OBJETIVOACERTADODEF, // Marca un objetivo como puntuado, para sumar la puntuación una única vez
-	nombreJugador: NOMBREJUGADORDEF // Nombre del jugador
+	nombreJugador: NOMBREJUGADORDEF, // Nombre del jugador
+	rachaAciertos: RACHADEF // Racha de aciertos
 };
 
 /**
@@ -41,6 +43,7 @@ function prepararJuego() {
 	juego.numObjetivoCorrecto = NUMOBJETIVOCORRECTODEF;
 	juego.puntos = PUNTOSDEF;
 	juego.objetivoPuntuado = OBJETIVOACERTADODEF;
+	juego.rachaAciertos = RACHADEF;
 }
 
 /**
@@ -105,12 +108,20 @@ function objetivoAcertado(numId) {
 	// Marcamos el objetivo como puntuado, y sumamos puntos
 	juego.objetivoPuntuado = true;
 	juego.puntos = juego.puntos + juego.puntosAcierto;
+	juego.rachaAciertos = juego.rachaAciertos + 1;
+
+	// Cuando se alcanza una racha de 10, los puntos conseguidos son x3
+	let resto = juego.rachaAciertos % 10;
+	if ( resto === 0 ){
+		juego.puntos = juego.puntos + juego.puntosAcierto * 3;
+	}
 
 	console.log('Objetivo ' + id + ' acertado');
 
 	// Mostramos la casilla como acertada
 	document.getElementById(id).classList.add(CLASEACIERTO);
 	document.getElementById(id).classList.remove(CLASEFALLO);
+	document.getElementById(IDRACHA).innerHTML = String(juego.rachaAciertos);
 }
 
 /**
@@ -126,12 +137,14 @@ function objetivoFallido(numId) {
 	// Con Math.abs() nos aseguramos que restamos puntos, evitando posibles problemas con dobles negativos
 	// Con Math.max() nos aseguramos que el total de puntos nunca es inferior a cero
 	juego.puntos = Math.max(juego.puntos - Math.abs(juego.puntosFallo), 0);
+	juego.rachaAciertos = 0;
 
 	console.log('Objetivo ' + id + ' fallido');
 
 	// Mostramos la casilla como fallida
 	document.getElementById(id).classList.add(CLASEFALLO);
 	document.getElementById(id).classList.remove(CLASEACIERTO);
+	document.getElementById(IDRACHA).innerHTML = String(juego.rachaAciertos);
 }
 
 /**
@@ -191,6 +204,7 @@ function inicializar() {
 	prepararJuego(); // Inicializamos el estado del juego a la posición de inicio
 	console.log('Juego inicializado');
 	nuevoObjetivo(); // Comenzamos a jugar
+	document.getElementById('botonIniciar').style.display = "none"; // Oculta el botón de iniciar juego
 }
 
 /**
@@ -209,9 +223,11 @@ function pulsado(numObjetivoPulsado) {
 	// Si es la primera vez que acierta un objetivo, registramos el acierto
 	if (!juego.objetivoPuntuado && acierto) {
 		objetivoAcertado(numObjetivoPulsado);
+		juego.objetivosPorMinuto = juego.objetivosPorMinuto + 1;
 	// Si es un fallo, registramos el fallo
 	} else if (!acierto) {
 		objetivoFallido(numObjetivoPulsado);
+		juego.objetivosPorMinuto = juego.objetivosPorMinuto - 1;
 	// Si no, es un acierto repetido
 	} else {
 		objetivoRepetido(numObjetivoPulsado);
@@ -221,4 +237,50 @@ function pulsado(numObjetivoPulsado) {
 	document.getElementById(IDPUNTUACION).innerHTML = String(juego.puntos);
 
 	return acierto;
+}
+
+/**
+ * Summary. Valida el formulario.
+ *
+ * Description. Valida el nombre, los puntos por acertar y los puntos por fallar para aplicarlo al juego.
+ *
+ */
+function validacionFormulario() {
+	const x = document.getElementById(IDFORMULARIO);
+
+	// Condiciones
+	const condicionAlfa = /^[0-9a-zA-Z]+$/;
+	const condicionPL = /^[A-Za-z]+$/;
+	const condicionPuntos = /^[0-9]+/;
+
+	// Validación de nombre
+	let nombre;
+	nombre = x.elements[0].value;
+	if (nombre.match(condicionAlfa) && nombre.charAt(0).match(condicionPL)){
+		juego.nombreJugador = nombre;
+		document.getElementById(IDNOMBRE).innerHTML = nombre;
+		document.getElementById('nameError').innerHTML = '';
+	} else {
+		document.getElementById('nameError').innerHTML = 'Inserte un nombre válido';
+	}
+
+	// Validación de puntos por acierto
+	let acierto;
+	acierto = x.elements[1].value;
+	if (acierto.match(condicionPuntos)){
+		juego.puntosAcierto = parseInt(acierto);
+		document.getElementById('pointsPerHitError').innerHTML = '';
+	} else {
+		document.getElementById('pointsPerHitError').innerHTML = 'Debe ser un número entero positivo';
+	}
+
+	// Validación de puntos por fallo
+	let fallo;
+	fallo = x.elements[2].value;
+	if (parseInt(fallo) <= parseInt(acierto) && fallo.match(condicionPuntos)){
+		juego.puntosFallo = parseInt(fallo);
+		document.getElementById('pointsPerMissError').innerHTML = '';
+	}  else {
+		document.getElementById('pointsPerMissError').innerHTML = 'No debe ser mayor que los puntos por acertar';
+	}
 }
